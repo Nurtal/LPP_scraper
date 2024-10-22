0976b006e891f984b205fc883ea3736676f86729
Nurtal <nathan.foulquier.pro@gmail.com>
2024-10-22 19:35:17 +0200

    update scrap

--- lpp_scrap.py
+++ lpp_scrap.py
@@ -4,8 +4,8 @@ import zipfile
 import os
 from io import BytesIO
 import shutil
-
-
+from bs4 import BeautifulSoup
+import re
 
 
 
@@ -43,6 +43,123 @@ def download_code_list()->list:
     # return codes
     return code_list
 
+
+def scrap_lpp_code(lpp_code):
+    """Extract information associated to a LPP code"""
+
+    # scrap page
+    url = f"http://www.codage.ext.cnamts.fr/cgi/tips/cgi-fiche?p_code_tips={lpp_code}&p_date_jo_arrete=%25&p_menu=FICHE&p_site=AMELI"
+    response = requests.get(url)
+
+    # Step 2: Check if the request was successful
+    m_list = []
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        stuff = soup.find_all('tr')
+
+        for s in stuff:
+            text = s.get_text()
+            # print(text)
+            # print("-"*45)
+            m_list.append(text)
+
+    else:
+        print(f"Failed to retrieve the page. Status code: {response.status_code}")
+
+
+    description_rank = -1
+    data = {
+        'code':lpp_code,
+        'désignation':'',
+        'description':'',
+        'tarif':'',
+        'prix_unitaire_réglementé':'',
+        'montant_max_remboursement':'',
+        'entente_prealable':'',
+        'indications':'',
+        'identifiant':'',
+        'age_max':'',
+        'nature_prestation':'',
+        'type_prestation':'',
+        'chapitre':''
+        
+    }
+    for x in range(len(m_list)):
+        text = m_list[x]
+
+
+        # print(text)
+        print("-"*45)
+
+        # hunt designation & description
+        if re.search('^Désignation', text):
+            d = text.split('Désignation:')
+            if len(d) > 1:
+                data['désignation'] = d[1]
+                description_rank = x+1
+            
+        # hunt tarif
+        if re.search('^Tarif:', text):
+            t = text.split('Tarif:')
+            if len(t) > 1:
+                t = t[1].split("\xa0")[0].replace(' ', '').replace(',', '.')
+                data['tarif'] = t
+
+        # hunt Prix unitaire réglementé
+        if re.search('^Prix unitaire réglementé', text):
+            p = text.split('Prix unitaire réglementé:')
+            if len(p) > 1:
+                p = p[1].replace('Néant', '')
+                data['prix_unitaire_réglementé'] = p
+
+        # hunt Montant max remboursement
+        if re.search('^Montant max remboursement', text):
+            m = text.split('Montant max remboursement:')
+            if len(m) > 1:
+                m = m[1].replace('Néant', '')
+                data['montant_max_remboursement'] = m
+
+        # hunt Entente préalable
+        if re.search('^Entente préalable', text):
+            e = text.split('Entente préalable:')
+            if len(e) > 1:
+                data['entente_prealable'] = e[1]
+
+        # hunt Indications
+        if re.search('^Indications', text):
+            i = text.split('Indications:')
+            if len(i) > 1:
+                data['indications'] = i[1]
+
+        # hunt Identifiant
+        if re.search('^Identifiant', text):
+            i = text.split('Identifiant:')
+            if len(i) > 1:
+                data['identifiant'] = i[1].replace('Néant', '')
+
+        # hunt Age max
+        if re.search('^Age max', text):
+            a = text.split('Age maxi:')
+            if len(a) > 1:
+                data['age_max'] = a[1].replace('Néant', '')
+
+        # hunt Nature prestation
+        if re.search('^Nature de prestation:', text):
+            n = text.split('Nature de prestation:')
+            print(n)
+            # if len(a) > 1:
+            #     data['age_max'] = a[1].replace('Néant', '')
+
+        # hunt type prestation
+
+        # hunt chapitre
+
+    # catch description
+    if description_rank != -1 and len(m_list) <= description_rank:
+        data['description'] = m_list[description_rank]
+
+        
+
     
 
     
@@ -51,4 +168,8 @@ def download_code_list()->list:
 
 if __name__ == "__main__":
 
-    code_list = download_code_list()    
+    # code_list = download_code_list()    
+    c = "1152380"
+    c = "3299070"
+
+    scrap_lpp_code(c)
