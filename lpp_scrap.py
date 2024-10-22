192a927023d899cbb1a050fd9d2ccd89a7c0950d
Nurtal <nathan.foulquier.pro@gmail.com>
2024-10-22 15:46:25 +0200

    dl lpp codes

--- lpp_scrap.py
+++ lpp_scrap.py
@@ -1,25 +1,47 @@
 from dbfread import DBF
+import requests
+import zipfile
+import os
+from io import BytesIO
+import shutil
 
 
 
 
 
-
-def download_code_list():
+def download_code_list()->list:
     """Download dbf file from AMELI and extract list of LPP code"""
+    
+    # download zip file from AMELI
+    version = "802"
+    dl_folder = "/tmp/lpp_db"
+    url = f"http://www.codage.ext.cnamts.fr/codif/tips/download_file.php?filename=tips/LPP{version}.zip"
+    response = requests.get(url)
+    if response.status_code == 200:
+        zip_file = BytesIO(response.content)
+        with zipfile.ZipFile(zip_file, 'r') as z:
+            extract_path = dl_folder
+            os.makedirs(extract_path, exist_ok=True)
+            z.extractall(extract_path)
+    else:
+        print(f"Failed to download the file. Status code: {response.status_code}")
 
-
-    # download file
-
-    # Spécifiez le chemin vers le fichier .dbf
-    file_path = 'LPP802/lpp_fiche_tot802.dbf'
-
-    # Ouvrir et lire le fichier DBF
+    # extract codes
+    code_list = []
+    file_path = f"{dl_folder}/lpp_fiche_tot{version}.dbf"
     table = DBF(file_path, encoding='unicode_escape')
-
-    # Parcourir les enregistrements dans la table
     for record in table:
-        print(record)  # Affiche c
+        if record['CODE_TIPS'] not in code_list:
+            code_list.append(record['CODE_TIPS'])
+
+    # delete tmp folder
+    try:
+        shutil.rmtree(dl_folder)
+    except OSError as e:
+        print(f"Error: {dl_folder} : {e.strerror}")
+
+    # return codes
+    return code_list
 
     
 
@@ -29,4 +51,4 @@ def download_code_list():
 
 if __name__ == "__main__":
 
-    
+    code_list = download_code_list()    
